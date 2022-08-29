a3f808df74a2f7d7d83098835b806a4a6543f26c
Thomas Dziemian <thomas.dziemian@gmail.com>
2022-08-29 21:08:49 +0200

    init

---
+++ src/test/java/com/tecs/bpa/BpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecs.bpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
