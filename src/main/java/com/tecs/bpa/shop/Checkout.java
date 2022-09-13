6d8ef8ce7675d91e4f7ec7eafdb1ea461e4db37e
Thomas Dziemian <thomas.dziemian@gmail.com>
2022-09-13 23:18:30 +0200

    refactored to private methods

--- src/main/java/com/tecs/bpa/shop/Checkout.java
+++ src/main/java/com/tecs/bpa/shop/Checkout.java
@@ -11,34 +11,44 @@ public class Checkout {
 
     public Order checkout(Cart cart, OrderInfo orderInfo) {
         Level level = orderInfo.getCustomer().getLevel();
-        ShipmentDetails shipmentDetails = new ShipmentDetails();
         Customer customer = orderInfo.getCustomer();
-        List<OrderItem> orderItemList = new ArrayList<>();
-        cart.getCartItemList().stream().forEach(p -> {
-            double orderPrice;
-            if (p.getAmount() > 5) {
-                orderPrice = p.getProductItem().getPrice() * p.getAmount() * AMOUNT_DISCOUNT;
-            } else {
-                orderPrice = p.getProductItem().getPrice() * p.getAmount();
-            }
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrderPrice(orderPrice);
-            if (p.getProductItem().isDangerousGoods()) {
-                shipmentDetails.setDangerousGoods(true);
-            }
-            orderItemList.add(orderItem);
-        });
 
-        double totalOrderItemPrice = orderItemList.stream().mapToDouble(o -> o.getOrderPrice()).sum();
+        //Obtain shipment details like costs, delivery date and so on
+        ShipmentDetails shipmentDetails = getShipmentDetails(orderInfo);
 
-        if (level == Level.GOLD) {
-            totalOrderItemPrice = totalOrderItemPrice * CUSTOMER_DISCOUNT;
-        }
+        //Create order items
+        List<OrderItem> orderItemList = getOrderItems(cart, shipmentDetails);
 
-        double shippingCosts;
-        LocalDate deliveryDate;
-        TransportMode transportMode;
+        //Calculate total order items price
+        double totalOrderItemPrice = getTotalOrderItemPrice(level, orderItemList);
+
+        //Create and return order
+        return createOrder(orderInfo, customer, shipmentDetails, orderItemList, totalOrderItemPrice);
+
+    }
+
+    private Order createOrder(
+            OrderInfo orderInfo, Customer customer, ShipmentDetails shipmentDetails,
+            List<OrderItem> orderItemList, double totalOrderItemPrice) {
         Order order = new Order();
+        order.setOrderItemList(orderItemList);
+        order.setCustomer(customer);
+        if (orderInfo.getRecipient() == null) {
+            Recipient recipient = new Recipient(customer.getName(), customer.getWorldZone());
+            shipmentDetails.setRecipient(recipient);
+        } else {
+            shipmentDetails.setRecipient(orderInfo.getRecipient());
+        }
+        order.setTotalPrice(totalOrderItemPrice);
+        order.setShipmentDetails(shipmentDetails);
+        return order;
+    }
+
+    private ShipmentDetails getShipmentDetails(OrderInfo orderInfo) {
+        TransportMode transportMode;
+        LocalDate deliveryDate;
+        double shippingCosts;
+        ShipmentDetails shipmentDetails = new ShipmentDetails();
         if (orderInfo.getRecipient().getWorldZone() == Zone.NORTH_AMERICA) {
             if (orderInfo.getShippingPriority() == OrderInfo.ShippingPriority.NEXT_DAY) {
                 shipmentDetails.setMaxShippingDate(LocalDate.now());
@@ -58,23 +68,38 @@ public class Checkout {
                 transportMode = TransportMode.AIR;
                 shipmentDetails.setMaxShippingDate(LocalDate.now().plusDays(2));
         }
-
-
-        order.setOrderItemList(orderItemList);
-        order.setCustomer(customer);
-        if (orderInfo.getRecipient() == null) {
-            Recipient recipient = new Recipient(customer.getName(), customer.getWorldZone());
-            shipmentDetails.setRecipient(recipient);
-        } else {
-            shipmentDetails.setRecipient(orderInfo.getRecipient());
-        }
-        order.setTotalPrice(totalOrderItemPrice);
         shipmentDetails.setDeliveryDate(deliveryDate);
         shipmentDetails.setShippingCosts(shippingCosts);
         shipmentDetails.setTransportMode(transportMode);
-        order.setShipmentDetails(shipmentDetails);
+        return shipmentDetails;
+    }
 
-        return order;
+    private double getTotalOrderItemPrice(Level level, List<OrderItem> orderItemList) {
+        double totalOrderItemPrice = orderItemList.stream().mapToDouble(o -> o.getOrderPrice()).sum();
+
+        if (level == Level.GOLD) {
+            totalOrderItemPrice = totalOrderItemPrice * CUSTOMER_DISCOUNT;
+        }
+        return totalOrderItemPrice;
+    }
+
+    private List<OrderItem> getOrderItems(Cart cart, ShipmentDetails shipmentDetails) {
+        List<OrderItem> orderItemList = new ArrayList<>();
+        cart.getCartItemList().stream().forEach(p -> {
+            double orderPrice;
+            if (p.getAmount() > 5) {
+                orderPrice = p.getProductItem().getPrice() * p.getAmount() * AMOUNT_DISCOUNT;
+            } else {
+                orderPrice = p.getProductItem().getPrice() * p.getAmount();
+            }
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrderPrice(orderPrice);
+            if (p.getProductItem().isDangerousGoods()) {
+                shipmentDetails.setDangerousGoods(true);
+            }
+            orderItemList.add(orderItem);
+        });
+        return orderItemList;
     }
 
 }
